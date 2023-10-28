1dade8a2bb3bf0f561201046dd0df633ee7d5789
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2023-10-28 20:33:18 +0200

    hecho el título y cambios de diseño

--- scripts/derrota.js
+++ scripts/derrota.js
@@ -1,5 +1,14 @@
+
 const volverAJugarButton = document.getElementById('volver-a-jugar');
 volverAJugarButton.addEventListener('click', () => {
   // Redirige a la página "index.html"
   window.location.href = 'index.html';
 });
+// Obtén la referencia al elemento <span> con el id "solucion"
+var solucionElement = document.getElementById("solucion");
+
+// Obtén la palabra de solución de tu variable palabraFinal
+var palabraSolucion = palabraFinal;
+
+// Inserta la palabra de solución en el elemento <span>
+solucionElement.innerHTML = palabraSolucion;
