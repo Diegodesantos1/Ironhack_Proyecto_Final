51b19d54c0f0715747fc0e21b4c41485e22191ff
Diego de Santos <91721855+Diegodesantos1@users.noreply.github.com>
2023-10-28 02:57:39 +0200

    Add files via upload

---
+++ scripts/victoria.js
@@ -0,0 +1,5 @@
+const volverAJugarButton = document.getElementById('volver-a-jugar');
+volverAJugarButton.addEventListener('click', () => {
+  // Redirige a la página "index.html"
+  window.location.href = 'index.html';
+});
