f0eba356212669dbce21995d7d6576b24516882c
Diegodesantos1 <diegodesantosdelrio@gmail.com>
2023-10-29 03:22:28 +0100

    muchas mejoras, falta usar las imágenes

--- scripts/derrota.js
+++ scripts/derrota.js
@@ -1,14 +1,10 @@
+// Importa la variable "resultado" del archivo "script.js"
+import { resultado } from './juego.js';
+
 
 const volverAJugarButton = document.getElementById('volver-a-jugar');
 volverAJugarButton.addEventListener('click', () => {
   // Redirige a la página "index.html"
   window.location.href = 'index.html';
 });
-// Obtén la referencia al elemento <span> con el id "solucion"
-var solucionElement = document.getElementById("solucion");
 
-// Obtén la palabra de solución de tu variable palabraFinal
-var palabraSolucion = palabraFinal;
-
-// Inserta la palabra de solución en el elemento <span>
-solucionElement.innerHTML = palabraSolucion;
---
+++ scripts/inicio.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const buttons = document.querySelectorAll('button[data-letters]');
+  
+    buttons.forEach((button) => {
+      button.addEventListener("click", () => {
+        const letters = button.getAttribute("data-letters");
+        // Redirige al usuario a juego.html
+        window.location.href = `juego.html?letters=${letters}`;
+      });
+    });
+  
+    // Obtén el valor de "letters" de la URL
+    const urlParams = new URLSearchParams(window.location.search);
+    const letters = urlParams.get("letters");
+  
+    // Actualiza el valor de "maxlength" del campo de entrada según el valor de "letters"
+    if (letters) {
+      const guessInput = document.getElementById("guess");
+      guessInput.setAttribute("maxlength", letters);
+    }
+  });
+  
